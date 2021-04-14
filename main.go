714573ef2f874b35a558a911d218fd59c7ea7cac
Oli <oli@Olis-Macbook-Pro.local>
2021-04-14 18:33:59 +0100

    can now query ampleforth for price,market_cap, 24h_vol, 24h_perc_change, last_updated

--- main.go
+++ main.go
@@ -16,29 +16,47 @@ func main() {
 	engine := models.Engine{
 		Metadata: []models.MappingMetadata{
 			{
-				Key: "bitcoin_price",
+				Key: "bitcoin",
 				Endpoints: []models.Endpoint{
 					{
-						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
-						JSONPaths: map[string]string{"usd_per_token": "$.bitcoin.usd"},
+						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true",
+						JSONPaths: map[string]string{
+							"usd_per_token": "$.bitcoin.usd",
+							"market_cap": "$.bitcoin.usd_market_cap",
+							"24h_vol": "$.bitcoin.usd_24h_vol",
+							"24h_perc_change": "$.bitcoin.usd_24h_change",
+							"last_updated": "$.bitcoin.last_updated_at",
+						},
 					},
 				},
 			},
 			{
-				Key: "ethereum_price",
+				Key: "ethereum",
 				Endpoints: []models.Endpoint{
 					{
-						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd",
-						JSONPaths: map[string]string{"usd_per_token": "$.ethereum.usd"},
+						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true",
+						JSONPaths: map[string]string{
+							"usd_per_token": "$.ethereum.usd",
+							"market_cap": "$.ethereum.usd_market_cap",
+							"24h_vol": "$.ethereum.usd_24h_vol",
+							"24h_perc_change": "$.ethereum.usd_24h_change",
+							"last_updated": "$.ethereum.last_updated_at",
+						},
 					},
 				},
 			},
 			{
-				Key: "ampleforth_price",
+				Key: "ampleforth",
 				Endpoints: []models.Endpoint{
 					{
-						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=ampleforth&vs_currencies=usd",
-						JSONPaths: map[string]string{"usd_per_token": "$.ampleforth.usd"},
+						Endpoint:  "https://api.coingecko.com/api/v3/simple/price?ids=ampleforth&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true",
+						JSONPaths: map[string]string{
+							"usd_per_token": "$.ampleforth.usd",
+							"market_cap": "$.ampleforth.usd_market_cap",
+							"24h_vol": "$.ampleforth.usd_24h_vol",
+							"24h_perc_change": "$.ampleforth.usd_24h_change",
+							"last_updated": "$.ampleforth.last_updated_at",
+						},
 					},
 				},
 			},
